72a6171f10a744dbfd76771fd6bff0faa0e9acbb
Lasote <lasote@gmail.com>
2015-10-27 13:10:39 +0100

    Using conan test command

--- build.py
+++ build.py
@@ -5,30 +5,15 @@ import platform
 import sys
 
 
-def system(command):
+def test(arguments):
+    command = "conan test %s" % arguments
     retcode = os.system(command)
     if retcode != 0:
-        raise Exception("Error while executing:\n\t %s" % command)
-
-
-def build_run_example(settings):
-    current_dir = os.getcwd()
-    sha = hashlib.sha1(settings).hexdigest()
-    build_folder = os.path.join(current_dir, "conan_tmp", sha)
-    shutil.copytree("test", build_folder)
-    try:
-        os.chdir(build_folder)
-        system('conan install %s' % (settings))
-        system('conan build')
-        system("cd bin && .%smd5" % (os.sep))
-    finally:
-        os.chdir(current_dir)
-
+        exit("Error while executing:\n\t %s" % command)
 
 if __name__ == "__main__":
-    system('conan export lasote/stable')
+    os.system('conan export lasote/stable')
 
-    shutil.rmtree("conan_tmp", ignore_errors=True)
     if platform.system() == "Windows":
         if len(sys.argv) != 2 or sys.argv[1] not in ["x86", "x86_64"]:
             print("Please, specify x86 or x86_64 as a parameter")
@@ -40,30 +25,30 @@ if __name__ == "__main__":
 
         compiler = '-s compiler="Visual Studio" -s compiler.version=12 '
         # Static
-        build_run_example(compiler + '-s arch='+arch+' -s build_type=Debug -s compiler.runtime=MDd -o OpenSSL:shared=False')
-        build_run_example(compiler + '-s arch='+arch+' -s build_type=Debug -s compiler.runtime=MTd -o OpenSSL:shared=False')
-        build_run_example(compiler + '-s arch='+arch+' -s build_type=Release -s compiler.runtime=MD -o OpenSSL:shared=False')
-        build_run_example(compiler + '-s arch='+arch+' -s build_type=Release -s compiler.runtime=MT -o OpenSSL:shared=False')
+        test(compiler + '-s arch='+arch+' -s build_type=Debug -s compiler.runtime=MDd -o OpenSSL:shared=False')
+        test(compiler + '-s arch='+arch+' -s build_type=Debug -s compiler.runtime=MTd -o OpenSSL:shared=False')
+        test(compiler + '-s arch='+arch+' -s build_type=Release -s compiler.runtime=MD -o OpenSSL:shared=False')
+        test(compiler + '-s arch='+arch+' -s build_type=Release -s compiler.runtime=MT -o OpenSSL:shared=False')
 
         # Shared
-        build_run_example(compiler + '-s arch='+arch+' -s build_type=Debug -s compiler.runtime=MDd -o OpenSSL:shared=True')
-        build_run_example(compiler + '-s arch='+arch+' -s build_type=Debug -s compiler.runtime=MTd -o OpenSSL:shared=True')
-        build_run_example(compiler + '-s arch='+arch+' -s build_type=Release -s compiler.runtime=MD -o OpenSSL:shared=True')
-        build_run_example(compiler + '-s arch='+arch+' -s build_type=Release -s compiler.runtime=MT -o OpenSSL:shared=True')
+        test(compiler + '-s arch='+arch+' -s build_type=Debug -s compiler.runtime=MDd -o OpenSSL:shared=True')
+        test(compiler + '-s arch='+arch+' -s build_type=Debug -s compiler.runtime=MTd -o OpenSSL:shared=True')
+        test(compiler + '-s arch='+arch+' -s build_type=Release -s compiler.runtime=MD -o OpenSSL:shared=True')
+        test(compiler + '-s arch='+arch+' -s build_type=Release -s compiler.runtime=MT -o OpenSSL:shared=True')
 
     else:  # Compiler and version not specified, please set it in your home/.conan/conan.conf (Valid for Macos and Linux)
         # Static x86
-        build_run_example('-s arch=x86 -s build_type=Debug -o OpenSSL:shared=False')
-        build_run_example('-s arch=x86 -s build_type=Release -o OpenSSL:shared=False')
+        test('-s arch=x86 -s build_type=Debug -o OpenSSL:shared=False')
+        test('-s arch=x86 -s build_type=Release -o OpenSSL:shared=False')
 
         # Shared x86
-        build_run_example('-s arch=x86 -s build_type=Debug -o OpenSSL:shared=True')
-        build_run_example('-s arch=x86 -s build_type=Release -o OpenSSL:shared=True')
+        test('-s arch=x86 -s build_type=Debug -o OpenSSL:shared=True')
+        test('-s arch=x86 -s build_type=Release -o OpenSSL:shared=True')
 
         # Static x86_64
-        build_run_example('-s arch=x86_64 -s build_type=Debug -o OpenSSL:shared=False')
-        build_run_example('-s arch=x86_64 -s build_type=Release -o OpenSSL:shared=False')
+        test('-s arch=x86_64 -s build_type=Debug -o OpenSSL:shared=False')
+        test('-s arch=x86_64 -s build_type=Release -o OpenSSL:shared=False')
 
         # Shared x86_64
-        build_run_example('-s arch=x86_64 -s build_type=Debug -o OpenSSL:shared=True')
-        build_run_example('-s arch=x86_64 -s build_type=Release -o OpenSSL:shared=True')
+        test('-s arch=x86_64 -s build_type=Debug -o OpenSSL:shared=True')
+        test('-s arch=x86_64 -s build_type=Release -o OpenSSL:shared=True')
--- test/conanfile.py
+++ test/conanfile.py
@@ -1,5 +1,6 @@
 from conans.model.conan_file import ConanFile
 from conans import CMake
+import os
 
 
 class DefaultNameConan(ConanFile):
@@ -17,3 +18,6 @@ class DefaultNameConan(ConanFile):
     def imports(self):
         self.copy(pattern="*.dll", dst="bin", src="bin")
         self.copy(pattern="*.dylib", dst="bin", src="lib")
+        
+    def test(self):
+        self.run("cd bin && .%smd5" % (os.sep))
