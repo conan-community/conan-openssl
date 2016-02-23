2d12ce154e0c33de4c96e50884bb15ef73e6450d
Lasote <lasote@gmail.com>
2016-02-23 23:00:09 +0100

    1.0.2f

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ import os
 
 class OpenSSLConan(ConanFile):
     name = "OpenSSL"
-    version = "1.0.2e"
+    version = "1.0.2f"
     settings = "os", "compiler", "arch", "build_type"
     url="http://github.com/lasote/conan-openssl"
     # https://github.com/openssl/openssl/blob/OpenSSL_1_0_2c/INSTALL
@@ -54,7 +54,7 @@ class OpenSSLConan(ConanFile):
             tools.download(self.source_tgz, "openssl.tar.gz")
             tools.unzip("openssl.tar.gz", ".")
         
-        tools.check_sha256("openssl.tar.gz", "e23ccafdb75cfcde782da0151731aa2185195ac745eea3846133f2e05c0e0bff")
+        tools.check_sha256("openssl.tar.gz", "932b4ee4def2b434f85435d9e3e19ca8ba99ce9a065a61524b429a9d5e9b2e9c")
         os.unlink("openssl.tar.gz")
 
     def config(self):
