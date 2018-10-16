67f90e93e0212d3e9759d9ffc387e40319d370b8
SSE4 <tomskside@gmail.com>
2018-10-16 15:27:19 +0700

    - set required CMake variables for find_package
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -15,6 +15,10 @@ class DefaultNameConan(ConanFile):
         if self.settings.os == "Android":
             cmake.definitions["CONAN_LIBCXX"] = ""
         cmake.definitions["USE_FIND_PACKAGE"] = use_find_package
+        cmake.definitions["OPENSSL_ROOT_DIR"] = self.deps_cpp_info["OpenSSL"].rootpath
+        cmake.definitions["OPENSSL_USE_STATIC_LIBS"] = not self.options["OpenSSL"].shared
+        if self.settings.compiler == 'Visual Studio':
+            cmake.definitions["OPENSSL_MSVC_STATIC_RT"] = 'MT' in str(self.settings.compiler.runtime)
 
         cmake.configure()
         cmake.build()
