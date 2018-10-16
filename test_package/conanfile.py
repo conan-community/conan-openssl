6cce8a8e46e6d96c5477fd4918e5aeaa5f3c9829
SSE4 <tomskside@gmail.com>
2018-10-16 14:46:22 +0700

    - do not mix find_package and CONAN_LIBS
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -9,14 +9,19 @@ class DefaultNameConan(ConanFile):
     settings = "os", "compiler", "arch", "build_type"
     generators = "cmake"
 
-    def build(self):
+    def _build_cmake(self, use_find_package):
         cmake = CMake(self)
 
         if self.settings.os == "Android":
             cmake.definitions["CONAN_LIBCXX"] = ""
+        cmake.definitions["USE_FIND_PACKAGE"] = use_find_package
 
         cmake.configure()
         cmake.build()
+
+    def build(self):
+        self._build_cmake(use_find_package=True)
+        self._build_cmake(use_find_package=False)
 
     def imports(self):
         self.copy(pattern="*.dll", dst="bin", src="bin", root_package="OpenSSL")
